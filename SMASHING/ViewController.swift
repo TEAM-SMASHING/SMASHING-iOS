cee940eedeb3f67f586f6ce7b6a4cee61919e169
Rudy-009 <rofxnaos@gmail.com>
2025-12-28 11:15:06 +0900

    [add] XCode Project 생성

---
+++ SMASHING/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SMASHING
+//
+//  Created by 이승준 on 12/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
+import SwiftUI
+@available(iOS 18.0, *)
+#Preview {
+    ViewController()
+}
