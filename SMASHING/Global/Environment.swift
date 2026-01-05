fdba0916085f9f3157b4872201d45cf0fbdd75cb
Rudy-009 <rofxnaos@gmail.com>
2026-01-05 13:39:28 +0900

    [add] Network 개발 환경 구축

---
+++ SMASHING/Global/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  Smashing-Assignment
+//
+//  Created by 이승준 on 12/30/25.
+//
+
+import Foundation
+
+enum Environment {
+    
+}
---
+++ SMASHING/Networks/DTO/EmptyDataDTO.swift
@@ -0,0 +1,10 @@
+//
+//  EmptyDataDTO.swift
+//  SMASHING
+//
+//  Created by 이승준 on 1/5/26.
+//
+
+import Foundation
+
+struct EmptyDataDTO {}
