4a9ef58c443c9ae199b80ea630b212c67ff65700
JIN <jin724596@gmail.com>
2026-01-06 23:41:35 +0900

    [feat] TabBarViewController 배열 생성

--- SMASHING/Presentation/Core/MainTabBarController.swift
+++ SMASHING/Presentation/Core/MainTabBarController.swift
@@ -34,28 +34,66 @@ final class TabBarSceneFactory: TabBarSceneFactoryProtocol {
 }
 
 final class MainTabBarController: UITabBarController {
-    
+
+    // MARK: - Properties
+
     private var factory: TabBarSceneFactoryProtocol = TabBarSceneFactory()
-    
+
     static weak var shared: MainTabBarController?
-    
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setTabBarAppearance()
-        
+        self.setTabBarAppearance()
+        self.setupViewControllers()
     }
-    
+
+    // MARK: - Setup
+
+    private func setupViewControllers() {
+        let homeVC = self.factory.makeScene(for: .home)
+        homeVC.tabBarItem = UITabBarItem(
+            title: "홈",
+            image: UIImage(resource: .icHomeUnselected),
+            selectedImage: UIImage(resource: .icHomeSelected)
+        )
+
+        let matchingSearchVC = self.factory.makeScene(for: .matchingSearch)
+        matchingSearchVC.tabBarItem = UITabBarItem(
+            title: "매칭 검색",
+            image: UIImage(resource: .icSearchUnselected),
+            selectedImage: UIImage(resource: .icSearchSelected)
+        )
+
+        let matchingManageVC = self.factory.makeScene(for: .matchingManage)
+        matchingManageVC.tabBarItem = UITabBarItem(
+            title: "매칭 관리",
+            image: UIImage(resource: .icTrophyUnselected),
+            selectedImage: UIImage(resource: .icTrophySelected)
+        )
+
+        let profileVC = self.factory.makeScene(for: .profile)
+        profileVC.tabBarItem = UITabBarItem(
+            title: "프로필",
+            image: UIImage(resource: .icProfileUnselected),
+            selectedImage: UIImage(resource: .icProfileSelected)
+        )
+
+        self.viewControllers = [homeVC, matchingSearchVC, matchingManageVC, profileVC]
+    }
+
     private func setTabBarAppearance() {
         let appearance = UITabBarAppearance()
         appearance.configureWithTransparentBackground()
         appearance.backgroundColor = .white
         appearance.shadowColor = .darkGray
-        
+
         let itemAppearance = UITabBarItemAppearance()
-        
+
         appearance.stackedLayoutAppearance = itemAppearance
-        tabBar.standardAppearance = appearance
-        tabBar.scrollEdgeAppearance = appearance
+        self.tabBar.standardAppearance = appearance
+        self.tabBar.scrollEdgeAppearance = appearance
     }
-    
+
 }
