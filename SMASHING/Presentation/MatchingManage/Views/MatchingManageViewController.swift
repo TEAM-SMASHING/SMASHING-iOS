afc67e0267cd451a54626be11c9a50cee2ff7561
JIN <jin724596@gmail.com>
2026-01-12 16:56:45 +0900

    [feat] UiPageViewController setUp

--- SMASHING/Presentation/MatchingManage/Views/MatchingManageViewController.swift
+++ SMASHING/Presentation/MatchingManage/Views/MatchingManageViewController.swift
@@ -7,7 +7,26 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class MatchingManageViewController: BaseViewController {
+    
+    //MARK: - Properties
+    
+    private var currentTabIndex: Int = 0
+    private var categories: [UIViewController] = []
+    
+    private let headerView = MatchingManageHeaderView()
+    
+    //MARK: - UI Components
+    
+    private lazy var pageViewController: UIPageViewController = {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        return pageViewController
+    }()
 
     // MARK: - Lifecycle
 
@@ -20,6 +39,46 @@ final class MatchingManageViewController: BaseViewController {
         super.setLayout()
     }
 
+}
+
+extension MatchingManageViewController: UIPageViewControllerDelegate {
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool
+    ) {
+        guard completed,
+              let currentVC = pageViewController.viewControllers?.first,
+              let index = self.categories.firstIndex(of: currentVC) else { return }
+
+        self.currentTabIndex = index
+        self.headerView.updateSelectedTab(index: index)
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource
+
+extension MatchingManageViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+        guard let index = self.categories.firstIndex(of: viewController),
+              index > 0 else { return nil }
+        return self.categories[index - 1]
+    }
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+        guard let index = self.categories.firstIndex(of: viewController),
+              index < self.categories.count - 1 else { return nil }
+        return self.categories[index + 1]
+    }
 }
 
 // MARK: - Preview
