9ab1c2cb646ba0fc6ad2304d600bfc6c2c368b3e
JIN <jin724596@gmail.com>
2026-01-12 16:26:29 +0900

    [feat] PageViewContrllers에 넣을 VC 생성

---
+++ SMASHING/Presentation/MatchingManage/Pages/MatchingConfirmedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MatchingConfirmedViewController.swift
+//  SMASHING
+//
+//  Created by JIN on 1/12/26.
+//
+
+import UIKit
+
+class MatchingConfirmedViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
