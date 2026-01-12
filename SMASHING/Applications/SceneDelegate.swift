b3ed77e4e8358ff86111ec99852b427807a16a0a
Rudy-009 <rofxnaos@gmail.com>
2026-01-12 15:37:38 +0900

    [feat] SceneDelegate에 Coordinator 적용

--- SMASHING/Applications/SceneDelegate.swift
+++ SMASHING/Applications/SceneDelegate.swift
@@ -9,18 +9,21 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    let factory: SceneFactory = AppSceneFactory()
     var window: UIWindow?
-
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigationController = UINavigationController()
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator?.start()
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TestViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
