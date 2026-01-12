a4089c1e0986b632f6e52a22a05bbd3b02c2b4ad
JIN <jin724596@gmail.com>
2026-01-12 17:02:50 +0900

    [feat] header Tab Type 만들기

--- SMASHING/Presentation/MatchingManage/Views/MatchingManageHeaderView.swift
+++ SMASHING/Presentation/MatchingManage/Views/MatchingManageHeaderView.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class MatchingManageHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Types
+    
+    enum Tab: Int, CaseIterable {
+        case received = 0
+        case sent = 1
+        case confirmed = 2
+        
+        var title: String {
+            switch self {
+            case .received: return "받은 요청"
+            case .sent: return "보낸 요청"
+            case .confirmed: return "매칭 확정"
+            }
+        }
     }
-    */
-
 }
