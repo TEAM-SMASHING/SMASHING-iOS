6e2f1f75d93908cce7824735b6775e27f1222f50
KuKaH <kukah99@gmail.com>
2026-01-12 17:20:19 +0900

    [feat] CompositionalLayout 설정

---
+++ SMASHING/Presentation/Home/HomeViewLayout.swift
@@ -0,0 +1,146 @@
+//
+//  HomeViewLayout.swift
+//  SMASHING
+//
+//  Created by 홍준범 on 1/11/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+enum HomeViewLayout: Int, CaseIterable {
+    case matching
+    case recommendedUser
+    case ranking
+}
+
+extension HomeViewLayout {
+    var defaultEdgeInsets: NSDirectionalEdgeInsets {
+        get {
+            NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        }
+    }
+    
+    var itemSize: NSCollectionLayoutSize {
+        switch self {
+            case .matching:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(202))
+        case .recommendedUser:
+            return NSCollectionLayoutSize(widthDimension: .absolute(166), heightDimension: .absolute(187))
+        case .ranking:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(62))
+        }
+    }
+    
+    var itemEdgeInsets: NSDirectionalEdgeInsets {
+        defaultEdgeInsets
+    }
+    
+    var groupSize: NSCollectionLayoutSize {
+        switch self {
+        case .matching:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(202))
+        case .recommendedUser:
+            return NSCollectionLayoutSize(widthDimension: .absolute(166), heightDimension: .absolute(187))
+        case .ranking:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(62))
+        }
+    }
+    
+    var groupEdgeInsets: NSDirectionalEdgeInsets {
+        defaultEdgeInsets
+    }
+    
+    var headerSize: NSCollectionLayoutSize? {
+        switch self {
+        case .matching:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(65))
+        case .recommendedUser:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(27))
+        case .ranking:
+            return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(27))
+        }
+    }
+    
+    var header: NSCollectionLayoutBoundarySupplementaryItem? {
+        guard let headerSize = self.headerSize else { return nil }
+        return NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+    }
+    
+    var headerEdgeInsets: NSDirectionalEdgeInsets {
+        switch self {
+        case .matching:
+            return NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        case .recommendedUser:
+            return NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        case .ranking:
+            return NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        }
+    }
+    
+    var sectionBehavior: UICollectionLayoutSectionOrthogonalScrollingBehavior {
+        switch self {
+        case .matching:
+            return .none
+        case .recommendedUser:
+            return .continuous //가로 스크롤
+        case .ranking:
+            return .none
+        }
+    }
+    
+    var sectionEdgeInsets: NSDirectionalEdgeInsets {
+        switch self {
+        case .matching:
+            return NSDirectionalEdgeInsets(top: 28, leading: 16, bottom: 40, trailing: 16)
+        case .recommendedUser:
+            return NSDirectionalEdgeInsets(top: 20, leading: 16, bottom: 40, trailing: 0)
+        case .ranking:
+            return NSDirectionalEdgeInsets(top: 20, leading: 16, bottom: 0, trailing: 16)
+        }
+    }
+    
+    var section: NSCollectionLayoutSection {
+        return self.createSection()
+    }
+    
+    private func createSection() -> NSCollectionLayoutSection {
+        var supplementaryItems: [NSCollectionLayoutBoundarySupplementaryItem] = []
+        
+        let item = NSCollectionLayoutItem(layoutSize: self.itemSize)
+        item.contentInsets = self.itemEdgeInsets
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: self.groupSize, subitems: [item])
+        group.contentInsets = self.groupEdgeInsets
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = self.sectionEdgeInsets
+        
+        section.orthogonalScrollingBehavior = self.sectionBehavior
+        
+        switch self {
+        case .matching:
+            break
+        case .recommendedUser:
+            section.interGroupSpacing = 12
+        case .ranking:
+            section.interGroupSpacing = 12
+        }
+        
+        if let header = self.header {
+            header.contentInsets = self.headerEdgeInsets
+            supplementaryItems.append(header)
+        }
+        
+        section.boundarySupplementaryItems = supplementaryItems
+        
+        return section
+    }
+    
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout(section: self.section)
+        return layout
+    }
+}
