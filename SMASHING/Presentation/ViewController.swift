26e6d323d626ea23f54f6795fcda4bbef23ba4de
JIN <jin724596@gmail.com>
2026-01-12 16:12:37 +0900

    [feat] viewController 생성

---
+++ SMASHING/Presentation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SMASHING
+//
+//  Created by 이승준 on 12/28/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .Background.canvas
+        
+        self.hideKeyboardWhenDidTap()
+    }
+}
+
+import SwiftUI
+@available(iOS 18.0, *)
+#Preview {
+    ViewController()
+}
