a3611e475b997b803f68390b2fcf783759293da8
KuKaH <kukah99@gmail.com>
2026-01-15 01:28:25 +0900

    [init] inital RankingView, RankingViewController

---
+++ SMASHING/Presentation/Ranking/RankingView.swift
@@ -0,0 +1,16 @@
+//
+//  RankingView.swift
+//  SMASHING
+//
+//  Created by 홍준범 on 1/15/26.
+//
+
+import UIKit
+import Combine
+
+import SnapKit
+import Then
+
+final class RankingView: UIView {
+    
+}
---
+++ SMASHING/Presentation/Ranking/RankingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RankingViewController.swift
+//  SMASHING
+//
+//  Created by 홍준범 on 1/15/26.
+//
+
+import UIKit
+import Combine
+
+import SnapKit
+import Then
+
+final class RankingViewController: BaseViewController {
+    
+}
