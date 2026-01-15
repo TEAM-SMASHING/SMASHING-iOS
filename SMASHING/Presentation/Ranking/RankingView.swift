b29adbbbb6259b2cf62057e0ba25a4c2cf1a1d4c
KuKaH <kukah99@gmail.com>
2026-01-16 01:21:10 +0900

    [feat] RankingView UI 구현

--- SMASHING/Presentation/Ranking/RankingView.swift
+++ SMASHING/Presentation/Ranking/RankingView.swift
@@ -11,6 +11,83 @@ import Combine
 import SnapKit
 import Then
 
-final class RankingView: UIView {
+final class RankingView: BaseUIView {
+    let navigationBar = CustomNavigationBar(title: "전체 랭킹")
     
+    let topThreePodium = TopThreePodium()
+    
+    private let backgroundEffectView = UIView().then {
+            $0.backgroundColor = .Tier.diamondBackground
+            $0.layer.cornerRadius = 50
+            $0.layer.shadowColor = UIColor(red: 27/255, green: 47/255, blue: 81/255, alpha: 1.0).cgColor
+            $0.layer.shadowOffset = CGSize(width: 0, height: 0)
+            $0.layer.shadowRadius = 100
+            $0.layer.shadowOpacity = 1.0
+            $0.layer.masksToBounds = false
+        }
+
+        override func layoutSubviews() {
+            super.layoutSubviews()
+
+            // Spread 효과를 위해 shadowPath 설정
+            let spread: CGFloat = 100
+            let rect = backgroundEffectView.bounds.insetBy(dx: -spread, dy: -spread)
+            backgroundEffectView.layer.shadowPath = UIBezierPath(roundedRect: rect, cornerRadius: backgroundEffectView.layer.cornerRadius + spread).cgPath
+        }
+    
+    private let collectionViewContainer = UIView().then {
+        $0.layer.cornerRadius = 16
+        $0.backgroundColor = .Background.canvas
+    }
+    
+    lazy var rankingCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 8
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.layer.cornerRadius = 16
+        return collectionView
+    }()
+    
+    override func setUI() {
+        collectionViewContainer.addSubview(rankingCollectionView)
+        
+        addSubviews(backgroundEffectView, navigationBar, topThreePodium, collectionViewContainer)
+    }
+    
+    override func setLayout() {
+        backgroundEffectView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(10)
+        }
+        
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        topThreePodium.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        collectionViewContainer.snp.makeConstraints {
+            $0.top.equalTo(topThreePodium.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide
+            )
+        }
+        
+        rankingCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(16)
+        }
+    }
+    
+    func registerCells() {
+        rankingCollectionView.register(RankingCell.self, forCellWithReuseIdentifier: RankingCell.reuseIdentifier)
+    }
 }
--- SMASHING/Presentation/Ranking/RankingViewController.swift
+++ SMASHING/Presentation/Ranking/RankingViewController.swift
@@ -11,6 +11,66 @@ import Combine
 import SnapKit
 import Then
 
-final class RankingViewController: BaseViewController {
+extension RankingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RankingCell", for: indexPath) as? RankingCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension RankingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width
+        return CGSize(width: width, height: 62)
+    }
+}
+
+final class RankingViewController: BaseViewController {
+    private let mainView = RankingView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDummyData()
+        view.backgroundColor = .Background.canvas
+        
+        mainView.registerCells()
+        mainView.rankingCollectionView.delegate = self
+        mainView.rankingCollectionView.dataSource = self
+    }
+    
+    private func setDummyData() {
+        mainView.topThreePodium.configure(
+            first: (
+                nickname: "밤이달이밤이달이밤이",
+                profileImage: nil,
+                rankImage: .icRank1,
+                tierImage: .bad,
+                lp: 1430
+            ),
+            second: (
+                nickname: "와구와구와구와구와구",
+                profileImage: nil,
+                rankImage: .icRank2,
+                tierImage: .good,
+                lp: 1298
+            ),
+            third: (
+                nickname: "스매싱고수스매싱고수",
+                profileImage: nil,
+                rankImage: .icRank3,
+                tierImage: .bad,
+                lp: 1156
+            )
+        )
+    }
 }
