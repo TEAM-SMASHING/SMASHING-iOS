48287db7de3bc274f7b9cc8bacf4bef57318c870
JIN <jin724596@gmail.com>
2026-01-17 04:08:22 +0900

    [feat] SentRequestDTO 구현

--- SMASHING/Presentation/MatchingManage/DTO/SentRequestResultDTO.swift
+++ SMASHING/Presentation/MatchingManage/DTO/SentRequestResultDTO.swift
@@ -5,4 +5,35 @@
 //  Created by JIN on 1/17/26.
 //
 
-import Foundation
+import UIKit
+
+struct SentRequestResultDTO: Codable {
+    let matchingID: String
+    let createdAt: Date
+    let status: String
+    let receiver: SentRequestReceiverDTO
+    
+    enum CodingKeys: String, CodingKey {
+        case matchingID = "matchingId"
+        case createdAt, status, receiver
+    }
+}
+
+// MARK: - Receiver
+struct SentRequestReceiverDTO: Codable {
+    let userID : String
+    let nickname: String
+    let gender: String
+    let reviewCount: Int
+    let tierID: Int
+    let tierName: String
+    let wins: Int
+    let losses: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case nickname, gender, reviewCount
+        case tierID = "tierId"
+        case tierName, wins, losses
+    }
+}
