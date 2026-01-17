e7c38b4d3c7fcfeb2075ec78e748461e24cbcbf4
JIN <jin724596@gmail.com>
2026-01-17 22:45:48 +0900

    [feat] Api String 적용

--- SMASHING/Networks/API/SentRequestResultAPI.swift
+++ SMASHING/Networks/API/SentRequestResultAPI.swift
@@ -5,41 +5,45 @@
 //  Created by JIN on 1/17/26.
 //
 
-import UIKit
+import Foundation
 
 import Moya
 import Alamofire
 
 enum SentRequestResultAPI {
-    case getSentRequestResult
-    case cancelSentRequest(resultId: String)
+    case getSentRequestList(snapshotAt: String?, cursor: String?, size: Int?)
+    case cancelSentRequest(matchingId: String)
 }
 
 extension SentRequestResultAPI: BaseTargetType {
-    
+
     var path: String {
         switch self {
-        case.getSentRequestResult:
-            return "api/v1/sent-requests"
-        case .cancelSentRequest:
-            return "api/v1/sent-requests"
+        case .getSentRequestList:
+            return "api/v1/users/me/matchings/sent"
+        case .cancelSentRequest(let matchingId):
+            return "api/v1/users/me/matchings/\(matchingId)/cancel"
         }
     }
-    
+
     var method: Moya.Method {
         switch self {
-        case.getSentRequestResult:
+        case .getSentRequestList:
             return .get
         case .cancelSentRequest:
-            return .delete
+            return .patch
         }
     }
-    
+
     var task: Moya.Task {
         switch self {
-        case .getSentRequestResult:
-            return .requestPlain
-        case .cancelSentRequest(resultId: let resultId):
+        case .getSentRequestList(let snapshotAt, let cursor, let size):
+            var parameters: [String: Any] = [:]
+            if let snapshotAt = snapshotAt { parameters["snapshotAt"] = snapshotAt }
+            if let cursor = cursor { parameters["cursor"] = cursor }
+            if let size = size { parameters["size"] = size }
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+        case .cancelSentRequest:
             return .requestPlain
         }
     }
