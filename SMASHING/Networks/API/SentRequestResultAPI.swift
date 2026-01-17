f9af1e11f9f6466b5e7d1fb34d53a7c0be97113d
JIN <jin724596@gmail.com>
2026-01-17 17:19:58 +0900

    [feat] SentRequestAPI 구현

--- SMASHING/Networks/API/SentRequestResultAPI.swift
+++ SMASHING/Networks/API/SentRequestResultAPI.swift
@@ -5,4 +5,42 @@
 //  Created by JIN on 1/17/26.
 //
 
-import Foundation
+import UIKit
+
+import Moya
+import Alamofire
+
+enum SentRequestResultAPI {
+    case getSentRequestResult
+    case cancelSentRequest(resultId: String)
+}
+
+extension SentRequestResultAPI {
+    
+    var path: String {
+        switch self {
+        case.getSentRequestResult:
+            return "api/v1/sent-requests"
+        case .cancelSentRequest:
+            return "api/v1/sent-requests"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case.getSentRequestResult:
+            return .get
+        case .cancelSentRequest:
+            return .delete
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getSentRequestResult:
+            return .requestPlain
+        case .cancelSentRequest(resultId: let resultId):
+            return .requestPlain
+        }
+    }
+}
