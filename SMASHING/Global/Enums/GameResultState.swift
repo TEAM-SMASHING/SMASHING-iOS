591fbc8226003b6838ea3f23ee28622986f93db3
JIN <jin724596@gmail.com>
2026-01-20 19:20:30 +0900

    [feat] GameResultState EnumType 추가

---
+++ SMASHING/Global/Enums/GameResultState.swift
@@ -0,0 +1,28 @@
+//
+//  GameResultState.swift
+//  SMASHING
+//
+//  Created by JIN on 1/19/26.
+//
+
+import Foundation
+
+enum GameResultStatus: String, Codable {
+    case pendingResult = "PENDING_RESULT"
+    case waitingConfirmation = "WAITING_CONFIRMATION"
+    case resultRejected = "RESULT_REJECTED"
+    case canceled = "CANCELED"
+    
+    var canSubmit: Bool {
+        switch self {
+        case .pendingResult, .resultRejected:
+            return true
+        case .waitingConfirmation, .canceled:
+            return false
+        }
+    }
+    
+    var isFirstSubmission: Bool {
+        return self == .pendingResult
+    }
+}
