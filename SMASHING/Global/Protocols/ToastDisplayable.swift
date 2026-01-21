340e36925fcee7b590f2af27d197e5df7e9e66d9
Rudy-009 <rofxnaos@gmail.com>
2026-01-22 08:18:14 +0900

    [feat] Toast Message 책임 분리

--- SMASHING/Global/Protocols/ToastDisplayable.swift
+++ SMASHING/Global/Protocols/ToastDisplayable.swift
@@ -5,4 +5,21 @@
 //  Created by 이승준 on 1/22/26.
 //
 
-import Foundation
+import UIKit
+
+protocol ToastDisplayable: AnyObject {
+    func showToast(type: NotificationType, completion: @escaping () -> Void)
+}
+
+extension ToastDisplayable where Self: UIViewController {
+    func showToast(type: NotificationType) {
+        let toast = ToastMessage()
+        self.view.addSubview(toast)
+        
+        toast.configure(title: type.displayText) {
+            toast.removeFromSuperview()
+        }
+        
+        toast.show()
+    }
+}
