72764a9076a37b38ef8c0f46f7306e4309e0c384
Rudy-009 <rofxnaos@gmail.com>
2026-03-02 20:30:27 +0900

    [feat] MyPageView 추가

--- SMASHING/Presentation/MyPage/MtPageView.swift
+++ SMASHING/Presentation/MyPage/MtPageView.swift
@@ -5,4 +5,227 @@
 //  Created by 이승준 on 2/27/26.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import SnapKit
+
+final class MyPageView: BaseUIView {
+    
+    // MARK: - Properties
+    
+    var leftButtonAction: (() -> Void)?
+    
+    // MARK: - UI Components
+    
+    private lazy var navigationBar = CustomNavigationBar(title: "마이페이지") {
+        self.leftButtonAction!()
+    }
+    
+    private let profileContainerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let profileImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.cornerRadius = 30
+        $0.clipsToBounds = true
+    }
+    
+    private let userNameLabel = UILabel().then {
+        $0.font = .pretendard(.textMdM)
+        $0.textColor = .Text.primary
+    }
+    
+    private let tierImage = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let profileStateStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .center
+        $0.layer.cornerRadius = 13
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.Border.tertiary.cgColor
+    }
+    
+    private let profileStateLabel = UILabel().then {
+        $0.font = .pretendard(.captionXsM)
+        $0.textColor = .Text.primary
+        $0.text = "내 프로필"
+    }
+    
+    private let accountManageContainerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let accountManageLabel = UILabel().then {
+        $0.text = "계정 관리"
+        $0.font = .pretendard(.captionXsM)
+        $0.textColor = .Text.tertiary
+    }
+    
+    private lazy var logoutButton = button(title: "로그아웃")
+    private lazy var signoutButton = button(title: "계정 탈퇴")
+    private lazy var signoutArrowButton = UIButton().then {
+        $0.contentMode = .scaleAspectFit
+        $0.setImage( .icArrowNext , for: .normal)
+    }
+    
+    private let divider = UIView().then {
+        $0.backgroundColor = UIColor.Background.surface
+    }
+    
+    private let policyContainerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let policyAndInformationLabel = UILabel().then {
+        $0.text = "정책 및 정보"
+        $0.font = .pretendard(.captionXsM)
+        $0.textColor = .Text.tertiary
+    }
+    
+    private lazy var privacyPolicyButton = button(title: "개인정보 처리 방침")
+    private lazy var termsOfServiceButton = button(title: "이용약관")
+    private let versionLabel = UILabel().then {
+        $0.text = "버전 정보"
+        $0.font = .pretendard(.textSmM)
+        $0.textColor = .Text.primary
+    }
+    private let versionInfoLabel = UILabel().then {
+        $0.font = .pretendard(.textSmM)
+        $0.textColor = .Text.tertiary
+        $0.text = "ver 0.0.0"
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func setUI() {
+        addSubviews(navigationBar, profileContainerView)
+        profileContainerView.addSubviews(profileImageView,
+                                         userNameLabel,
+                                         tierImage,
+                                         profileStateStack)
+        profileStateStack.addArrangedSubview(profileStateLabel)
+        addSubview(accountManageContainerView)
+        accountManageContainerView.addSubviews(accountManageLabel,
+                                               logoutButton,
+                                               signoutButton,
+                                               signoutArrowButton)
+        addSubviews(divider, policyContainerView)
+        policyContainerView.addSubviews(policyAndInformationLabel, privacyPolicyButton, termsOfServiceButton, versionLabel, versionInfoLabel)
+    }
+    
+    override func setLayout() {
+        navigationBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(safeAreaLayoutGuide)
+        }
+        
+        profileContainerView.snp.makeConstraints { make in
+            make.top.equalTo(navigationBar.snp.bottom).offset(24)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(60)
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.leading.verticalEdges.equalToSuperview()
+            make.width.equalTo(60)
+        }
+        
+        userNameLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(4)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(20)
+        }
+        
+        tierImage.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-4)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(20)
+            make.height.equalTo(24)
+            make.width.equalTo(67)
+        }
+        
+        profileStateStack.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(26)
+            make.width.equalTo(77)
+        }
+        
+        accountManageContainerView.snp.makeConstraints { make in
+            make.top.equalTo(profileContainerView.snp.bottom).offset(40)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(97)
+        }
+        
+        accountManageLabel.snp.makeConstraints { make in
+            make.leading.top.equalToSuperview()
+        }
+        
+        logoutButton.snp.makeConstraints { make in
+            make.top.equalTo(accountManageLabel.snp.bottom).offset(19.5)
+        }
+        
+        signoutButton.snp.makeConstraints { make in
+            make.top.equalTo(logoutButton.snp.bottom).offset(8)
+            make.leading.equalToSuperview()
+        }
+        
+        signoutArrowButton.snp.makeConstraints { make in
+            make.centerY.equalTo(signoutButton)
+            make.trailing.equalToSuperview()
+            make.size.equalTo(24)
+        }
+        
+        divider.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(accountManageContainerView.snp.bottom).offset(26)
+        }
+        
+        policyContainerView.snp.makeConstraints { make in
+            make.top.equalTo(divider.snp.bottom).offset(26)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(122)
+        }
+        
+        policyAndInformationLabel.snp.makeConstraints { make in
+            make.leading.top.equalToSuperview()
+        }
+        
+        privacyPolicyButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(policyAndInformationLabel.snp.bottom).offset(16)
+        }
+        
+        termsOfServiceButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(privacyPolicyButton.snp.bottom).offset(8)
+        }
+        
+        versionLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(termsOfServiceButton.snp.bottom).offset(8)
+        }
+        
+        versionInfoLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(versionLabel)
+            make.trailing.equalToSuperview()
+        }
+    }
+    
+    private func button(title: String) -> UIButton {
+        return UIButton().then {
+            $0.titleLabel?.font = .pretendard(.textSmM)
+            $0.setTitleColor(.Text.primary, for: .normal)
+            $0.setTitle(title, for: .normal)
+        }
+    }
+}
+
+
+import SwiftUI
+@available(iOS 18.0, *)
+#Preview {
+    MyPageView()
+}
