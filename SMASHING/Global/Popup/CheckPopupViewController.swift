adc906daa63513cb71e76a911e209ca6a781e201
Rudy-009 <rofxnaos@gmail.com>
2026-03-18 00:33:30 +0900

    [feat] 로그인 팝업 연결

--- SMASHING/Global/Popup/CheckPopupViewController.swift
+++ SMASHING/Global/Popup/CheckPopupViewController.swift
@@ -13,25 +13,25 @@ import Then
 final class CheckPopupViewController: DimmedViewController {
     
     // MARK: - UI Components
-
+    
     private let containerView = UIView().then {
         $0.backgroundColor = .Background.surface
         $0.layer.cornerRadius = 16
     }
-
+    
     private let titleLabel = UILabel().then {
         $0.font = .pretendard(.textMdSb)
         $0.textColor = .Text.primary
         $0.textAlignment = .center
     }
-
+    
     private let messageLabel = UILabel().then {
         $0.font = .pretendard(.textSmR)
         $0.textColor = .Text.tertiary
         $0.textAlignment = .center
         $0.numberOfLines = 0
     }
-
+    
     private lazy var confirmButton = UIButton().then {
         $0.setTitleColor(.Text.muted, for: .normal)
         $0.titleLabel?.font = .pretendard(.textSmR)
@@ -46,39 +46,38 @@ final class CheckPopupViewController: DimmedViewController {
         target: self,
         action: #selector(handleBackgroundTap)
     )
-
+    
     var onCancelTapped: (() -> Void)?
-    var onConfirmTapped: (() -> Void)?
-
+    lazy var onConfirmTapped: (() -> Void)? = {
+        self.dismiss(animated: true)
+    }
+    
     private let popupTitle: String
     private let popupMessage: String
-    private let cancelTitle: String
     private let confirmTitle: String
-
+    
     // MARK: - Initialize
 
     init(
         title: String,
         message: String,
-        cancelTitle: String,
         confirmTitle: String
     ) {
         self.popupTitle = title
         self.popupMessage = message
-        self.cancelTitle = cancelTitle
         self.confirmTitle = confirmTitle
         super.init()
         setUI()
         setLayout()
         view.addGestureRecognizer(tapGesture)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Setup Methods
-
+    
     override func setUI() {
         view.addSubview(containerView)
         containerView.addSubviews(titleLabel, messageLabel, confirmButton)
@@ -91,14 +90,14 @@ final class CheckPopupViewController: DimmedViewController {
         containerView.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.height.equalTo(153)
-            $0.width.greaterThanOrEqualTo(305)
+            $0.horizontalEdges.equalToSuperview().inset(16)
         }
-
+        
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(24)
             $0.horizontalEdges.equalToSuperview().inset(16)
         }
-
+        
         messageLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom)
             $0.horizontalEdges.equalToSuperview().inset(42)
@@ -106,7 +105,7 @@ final class CheckPopupViewController: DimmedViewController {
         
         confirmButton.snp.makeConstraints {
             $0.top.equalTo(messageLabel.snp.bottom).offset(20)
-            $0.horizontalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(16)
             $0.height.equalTo(40)
         }
     }
--- SMASHING/Presentation/Login/LoginViewController.swift
+++ SMASHING/Presentation/Login/LoginViewController.swift
@@ -31,7 +31,24 @@ final class LoginViewController: BaseViewController {
         
         mainView.kakaoLoginAction = { [weak self] in
             guard let self else { return }
-            input.send(.loginButtonTapped)
+            // input.send(.loginButtonTapped)
+            checkAutoUpdate()
         }
+    }
+    
+    private func checkAutoUpdate() {
+        let popup = CheckPopupViewController(
+            title: "최신 버전 업데이트",
+            message: "최신 버전 업데이트를 위해 스토어로 이동합니다.",
+            confirmTitle: "확인"
+        )
+
+        popup.onConfirmTapped = { [weak self] in
+            // Input/Output 패턴 추가
+        }
+        
+        print("check auto update")
+
+        present(popup, animated: true)
     }
 }
